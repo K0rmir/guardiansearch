fd33efa2294a199ac87a2cd2025628be71498c69
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-04-03 12:17:12 +0100

    add css to search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { PrimeReactProvider } from 'primereact/api';        
 import Home from "@/components/Home"
 
 export default function App() {
 
   return (
 
-    <div>
+    <PrimeReactProvider >
       <Home></Home>
-    </div>
+    </PrimeReactProvider>
 
  
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,37 @@
+import {InputText} from "primereact/inputtext";
 import apiCallSearch from "@/lib/apiCall";
+import "../styles/Home.css";
+import "primeicons/primeicons.css";
 
 export default function Home() {
   async function handleApiCallSearch(formData) {
     "use server";
     const searchQuery = formData.get("searchQuery");
     apiCallSearch(searchQuery);
-    console.log(searchQuery);
   }
 
   return (
     <div>
       <p>Guardian Search</p>
 
-      <form action={handleApiCallSearch}>
-        <input type="text" name="searchQuery" id="searchQuery" />
-        <button type="submit">Search</button>
-      </form>
+      <div className="formContainer">
+        <div className="searchForm">
+          <form action={handleApiCallSearch}>
+            <InputText
+              className="searchInput"
+              type="text"
+              name="searchQuery"
+              id="searchQuery"
+              placeholder="Search articles..."
+            />
+            <button type="submit" className="searchBtn">
+              <i
+                className="pi pi-search"
+                style={{fontSize: "1.5rem", color: "grey"}}></i>
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
