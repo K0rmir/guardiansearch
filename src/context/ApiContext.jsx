1766631e444253144b2f0201dfffa59e15ea3126
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-04-07 00:15:14 +0100

    update .env

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {createContext, useState, useContext} from "react";
+import {unstable_noStore as noStore} from "next/cache";
 
 // Get api key from environment variables. //
 // API Key is needed in every request. //
@@ -8,7 +9,8 @@ import {createContext, useState, useContext} from "react";
 const ApiContext = createContext();
 
 export default function ApiProvider({children}) {
-  const api_key = process.env.API_KEY;
+  noStore();
+  const api_key = process.env.NEXT_PUBLIC_API_KEY;
 
   const [articles, setArticles] = useState([]);
 
