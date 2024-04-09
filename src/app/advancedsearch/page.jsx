367a47bfb0d8a8ad15e8df2668216964f602eaa2
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-04-09 16:25:49 +0100

    add article table for article rendering

--- src/app/advancedsearch/page.jsx
+++ src/app/advancedsearch/page.jsx
@@ -8,18 +8,14 @@ import {Card} from "primereact/card";
 import {Button} from "primereact/button";
 import {useApiContext} from "@/context/ApiContext";
 import {useRouter} from "next/navigation";
+import Link from "next/link";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
 
 export default function AdvancedSearch() {
   const router = useRouter();
-  const {
-    searchQuery,
-    setSearchQuery,
-    searchDate,
-    setSearchDate,
-    apiCallAdvancedSearch,
-    bodySearch,
-    setBodySearch,
-  } = useApiContext();
+  const {apiCallAdvancedSearch, bodySearch, setBodySearch, articles} =
+    useApiContext();
 
   // const [checked, setChecked] = useState(false);
 
@@ -35,6 +31,45 @@ export default function AdvancedSearch() {
     console.log(formTag);
     apiCallAdvancedSearch(formQuery, formDate, formTag);
   }
+
+  // Article Table //
+
+  // Table Header //
+  const header = (
+    <div className="articleTableHeader">
+      <span className="headerTitle">Search results for: </span>
+    </div>
+  );
+  // Table Footer //
+  const footer = `Returned ${
+    articles ? articles.length : 0
+  } articles total by relevance to search query.`;
+  // Article Title Template //
+  const titleBodyTemplate = (articles) => {
+    return (
+      <Link href={`${articles.webUrl}`} className="articleTableTitle">
+        <p>{articles.webTitle}</p>
+      </Link>
+    );
+  };
+  // Article Image Template //
+  const imageBodyTemplate = (articles) => {
+    if (articles.elements) {
+      return (
+        <img
+          src={articles.elements[0].assets[0].file}
+          className="articleImg"></img>
+      );
+    } else return <p>No image available.</p>;
+  };
+  // Article Published Date Template //
+  const publishedBodyTemplate = (articles) => {
+    return <p>{new Date(articles.webPublicationDate).toLocaleDateString()}</p>;
+  };
+  // Article Category Template //
+  const categoryBodyTemplate = (articles) => {
+    return <p>{articles.sectionName}</p>;
+  };
 
   return (
     <div>
@@ -76,6 +111,40 @@ export default function AdvancedSearch() {
           </div>
         </form>
       </Card>
+      {/* Article Table */}
+      <div className="articleTableContainer">
+        <Card className="articleTableCard">
+          <DataTable
+            value={articles}
+            paginator
+            rows={5}
+            rowsPerPageOptions={[5, 10, 20, 30]}
+            scrollable
+            scrollHeight="750px"
+            header={header}
+            footer={footer}
+            className="articleTable">
+            <Column
+              header="Title"
+              body={titleBodyTemplate}
+              style={{width: "45%"}}></Column>
+            <Column
+              header="Image"
+              body={imageBodyTemplate}
+              style={{width: "15%"}}></Column>
+            <Column
+              header="Category"
+              body={categoryBodyTemplate}
+              className="column"
+              style={{width: "15%"}}></Column>
+            <Column
+              header="Published"
+              body={publishedBodyTemplate}
+              className="column"
+              style={{width: "15%"}}></Column>
+          </DataTable>
+        </Card>
+      </div>
     </div>
   );
 }
