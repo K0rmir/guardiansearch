84eefde0392ec3ed26e62e1b2a4f51bdd1433d7f
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-05-08 20:18:01 +0100

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/',
-    '/api/webhooks(.*)']
-})
+  publicRoutes: ['/'],
+  ignoredRoutes: ['/api/webhooks']
+});
 
 
 
