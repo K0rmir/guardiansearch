63c7c6ee52ba054b0fdb97d7e78655a8bd6bbd37
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-05-08 20:09:29 +0100

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/',
+    '/api/webhooks']
+})
+
+
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
